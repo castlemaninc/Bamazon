d2a588a8ce5d2807d2d7bba1733ce9fdb53f2e4e
Bryan Castleman <Bryan Castleman>
2017-04-21 23:34:45 -0700

    adding an improved SQL query that I intend to improve upon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,34 +21,16 @@ connection.connect(function(err) {
 	fillTable();	
 });
 
-// Pull data from CSV file
-// fs.readFile("productData.csv", "utf8", function(error, data) {
 
-//   // We will then print the contents of data
-//   // console.log(data);
-
-//   // Then split it by commas (to make it more readable)
-//   var dataArr = data.split("\r\n");
-//   // We will then re-display the content as an array for later use.
-//   // console.log(dataArr);
-//   var item1 = (dataArr[0].split(","));
-//   console.log(item1);
-
-// });
-
-
-// display items from database 
+// Creates a CLI table for Bamazon products with column headings
 var table = new Table({
     head: ['ID', 'Product', 'Department', 'Price', 'Stock'], 
     colWidths: [5, 30, 30, 30, 10]
 });
 
-// table.push(
-//     [1, "hat", "sports", 5.66, 5],
-//     [2, "sandals", "men", 10.66, 25]
-// );
-
+// A function that fills the CLI table with MySQL data
 var fillTable = function(){
+
 	var query = "SELECT * FROM bamazon_db.products";
 
 	connection.query(query, function(err, res) {
@@ -65,20 +47,21 @@ var fillTable = function(){
 		}			
     	displayItems();
     });
-	
+
 }
 
-
+// Displays the CLI table 
 var displayItems = function(){
 	console.log(table.toString());
 	chooseByID();	
 };
 
-
+// Prompts customer to choose an item by ID number and asks for the quantity the customer would like to buy
+// Then it checks if the store has enough of the product to meet the customer's request.
 var chooseByID = function(){
 	inquirer.prompt([{
 	name: "idNum",
-	message: "Please type in the item number of the product you would like",
+	message: "Please type in the item number of the product you would like to purchase.",
 	validate: function(value) {
 		if (isNaN(value) === false) {
 			return true;
@@ -95,29 +78,46 @@ var chooseByID = function(){
 	   	return false;
 	   	}
 	}]).then(function(answer){
-		// var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-		var query = "SELECT * FROM bamazon_db.products";
+		
+		
 
 		idNum = parseInt(answer.idNum);
 		stockNum = parseInt(answer.stockNum);
-		console.log(idNum);
-		console.log(stockNum);
+		console.log("This is idNum " + idNum);
+		console.log("This is stockNum " + stockNum);
 
-		connection.query(query, [answer.start, answer.end], function(err, res) {
+		var query = "SELECT * FROM products WHERE item_id = 3";
+
+		connection.query(query, function(err, res) {
 			// console.log("The type of item id is:" + typeof res[0].item_id);
 			// console.log("The type of idNum is:" + typeof idNum);
-			for(i=0;i<res.length;i++){
-				if (res[i].item_id === idNum){
-				console.log("This is your item: " + res[i].item_id + res[i].product_name);
-				table.push([res[i].item_id,
-							res[i].product_name,
-							res[i].department_name,
-							res[i].price,
-							res[i].stock_quantity
-				]);
-				displayItems();
-				}
-			}			
+			if (err){
+				throw err;
+			} else {
+				console.log(res);
+			}
+			
+
+			// console.log("res.length = " + res.length);
+			// for(i=0;i<res.length;i++){
+
+			// 	if (idNum === res[i].item_id  && stockNum < res[i].stock_quantity){
+			// 		console.log("Comparing " + res[i].item_id  + " and " + idNum );
+			// 		console.log("You selected item number " + res[i].item_id + ". We have " + res[i].stock_quantity + " in stock.");
+			// 		// chooseByID();
+			// 		// Update the SQL database to reflect the remaining quantity.
+			// 		// Once the update goes through, show the customer the total cost of their purchase.
+					
+			// 		chooseByID();
+			// 		return;
+			// 	} else {					
+			// 		console.log("Insufficient Quantity!");
+			// 		console.log("Comparing " + idNum + " and " + res[i].item_id);
+			// 		console.log("You selected item number " + res[i].item_id + ". We have " + res[i].stock_quantity + " in stock.");
+			// 		chooseByID();
+			// 		return;									
+			// 	}
+			// }			
 	    	// chooseByID();
 	    });
 	});
